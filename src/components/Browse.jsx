707945d89f29dfd8059f8476490841b23cb8b2eb
Talha Ayub <103215272+Talhaayubkhan@users.noreply.github.com>
2025-02-08 21:32:38 +0500

    add hooks for popular, trending, and upcoming movies in Browse component

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,10 +1,15 @@
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import usePopularMovies from "../hooks/usePopularMovies";
+import useTrendingMovies from "../hooks/useTrendingMovies";
+import useUpcommingMovies from "../hooks/useUpcomingMovies";
 import Header from "./Header";
 import MainVideoContainer from "./MainVideoContainer";
 import SecondaryContainer from "./SecondaryContainer";
 const Browse = () => {
   useNowPlayingMovies();
-
+  usePopularMovies();
+  useTrendingMovies();
+  useUpcommingMovies();
   return (
     <div>
       <Header />
